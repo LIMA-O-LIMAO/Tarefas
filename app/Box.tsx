84d609c6e1a9e4c4261fe79f2a7e85e91133d8eb
LIMA-O-LIMAO <joaovitorlima0411@gmail.com>
2024-02-11 00:58:48 -0300

    Inicio

---
+++ app/Box.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+
+
+ export const Estilos= styled.nav`
+
+background-color: #adadad;
+border: 10px;
+border-radius: 10px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+width: 500px;
+
+
+
+`
+export default Estilos;
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,54 @@
+'use client'
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import Estilos from './Box'
+import { useState } from "react";
+import test from "node:test";
+
+
+
+
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  const [Tarefas,setTarefas]=useState([])
+  const [testk,setTesk] = useState("")
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+function Adicionar(){
+setTarefas([...Tarefas,testk])
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+setTesk('')
+
+
+
+console.log(Tarefas)
+}
+return (
+<main>
+<Estilos>
+ <h1>Tarefas</h1>
+
+<div>
+
+<p></p>
+</div>
+
+
+
+<label>Digite sua atividade</label>
+<input type="text"
+value={testk}
+onChange={(e) => setTesk(e.target.value)}
+
+/>
+<button onClick={Adicionar}> Adicionar </button>
+
+</Estilos>
+
+</main>
+
   );
 }
